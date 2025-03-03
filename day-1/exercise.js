a49acd693303578a69a5e93b73acb8b91ffd470c
Arun Singh <arunsingh28aug.as@gmail.com>
2025-03-03 16:31:44 +0530

    Add initial implementations for simple interest, max of two numbers, factorial, and age eligibility for driver's license

---
+++ day-1/exercise.js
@@ -0,0 +1,23 @@
+// calculate simple interest
+function findSI(p, r, t) {
+  return p * r * t;
+} // si = P*R*T/100
+
+// find the max of 2 number
+function findMaxOf2Numbers(a, b) {
+  return a > b;
+}
+
+// calculate factorial of n
+function findFactorial(n) {
+  var result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+// get the DL or not based on the age
+function isElegbalForDL(age) {
+  return age >= 18 ? true : false;
+}
