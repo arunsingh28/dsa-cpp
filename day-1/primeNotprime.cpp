bd1e4d0bd3e0a96a60d37c0d4f5595d31e77c845
Arun Singh <arunsingh28aug.as@gmail.com>
2025-03-04 07:14:48 +0530

    Add function to find largest prime number and optimize prime check logic

--- day-1/primeNotprime.cpp
+++ day-1/primeNotprime.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// find the largest prime number in range of (2 - n)
+int findMaxPrime(int n){
+    int max,i;
+    for(i=2; i <= n-1; i++){
+        if(n%i == 0){
+           max = i;
+        }else{
+            
+        }
+    }
+    return max;
+}
+
 // check number is prime or not
 int main(){
     int N,i;
     bool isPrime = true;
     cout << "Enter the number: ";
     cin >> N;
-    for(i=2; i <= N-1; i++){
+    for(i=2; i*i <= N; i++){
         if(N%i == 0){
             isPrime = false;
             break;
@@ -17,6 +30,7 @@ int main(){
     }
     if(isPrime == true){
         cout << N <<" is a prime number\n";
+        cout << "Bigest prime number is "<< findMaxPrime(N) <<endl;
     }else{
         cout << N <<" is not prime number\n";
     }
