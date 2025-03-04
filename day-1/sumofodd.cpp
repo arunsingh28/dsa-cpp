e7958afea4f3c11c46ff847c6a921cf7f570625c
Arun Singh <arunsingh28aug.as@gmail.com>
2025-03-04 06:44:56 +0530

    Comment out sum of odd numbers logic in sumofodd.cpp for future reference

--- day-1/sumofodd.cpp
+++ day-1/sumofodd.cpp
@@ -7,11 +7,27 @@ int main(){
     int N,i,sum=0;
     cout << "Enter the nth number: ";
     cin >> N;
-    for(i=1; i <= N; i++){
-        if(i%2 != 0){
-            sum+=i;
-        }
-    }
+
+    // for(i=1; i <= N; i++){
+    //     if(i%2 != 0){
+    //         sum += i;
+    //     }
+    // }
+
+    // while(i <= N){
+    //     if(i%2 !=0){
+    //         sum += i;
+    //     }
+    //     i++;
+    // }
+
+    // do {
+    //     if(i%2 != 0){
+    //         sum += i;
+    //     }
+    //     i++;
+    // } while( i <= N);
+
     cout << "Sum of the nth odd number is: "<<sum<<endl;
     return 0;
 }
